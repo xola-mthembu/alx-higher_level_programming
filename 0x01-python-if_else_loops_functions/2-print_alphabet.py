488d5967693c7819c55af78f484b5e5858019b3c
xola-mthembu <xola.mthembu@gmail.com>
2024-01-02 15:45:00 -0800

    2-print_alphabet.py updated

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
-# Using a loop to print each letter in the ASCII lowercase alphabet
-for letter in range(97, 123):  # ASCII values for 'a' to 'z'
-    print(f"{chr(letter)}", end='')
+# Constructing the alphabet string using a loop
+alphabet = ''.join([chr(letter) for letter in range(97, 123)])
+
+# Printing the entire alphabet string in one go
+print(f"{alphabet}", end='')
