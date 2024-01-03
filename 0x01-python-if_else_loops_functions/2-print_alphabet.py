0b53af0180f2e4d172965a3a38a3bf6e260b32eb
xola-mthembu <xola.mthembu@gmail.com>
2024-01-02 16:00:17 -0800

    2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+# This program prints the ASCII alphabet in lowercase, not followed by a new line.
+# It uses only one print function with string format, one loop, and tab indentations.
 
-# Directly printing each letter in the loop
-for letter in range(97, 123):  # ASCII values from 'a' (97) to 'z' (122)
-	print(f"{chr(letter)}", end='')
+# Loop to print each letter
+for letter in range(97, 123):
+    print("{}".format(chr(letter)), end='')
