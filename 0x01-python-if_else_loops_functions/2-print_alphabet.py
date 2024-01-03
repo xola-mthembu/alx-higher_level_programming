675ff16e36911d4c646adaccbd766140861afe82
xola-mthembu <xola.mthembu@gmail.com>
2024-01-02 16:05:03 -0800

    2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # This program prints the ASCII alpha in lowercase, not followed by a new line.
-# It uses only one print funct with string format, one loop, and tab indentations.
+# It uses only 1 print funct with string format, 1 loop, and tab indentations.
 
 # Loop to print each letter
 for letter in range(97, 123):
