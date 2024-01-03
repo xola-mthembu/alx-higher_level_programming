c0a1dd84d9a87d9ed36d9c11c699626fedbfdbfb
xola-mthembu <xola.mthembu@gmail.com>
2024-01-02 17:25:18 -0800

    100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,2 +1,3 @@
 #!/usr/bin/python3
-print("{}".format("".join(chr(i) if i % 2 == 0 else chr(i - 32) for i in range(122, 96, -1))), end='')
+alphabet = "".join(chr(i) if i % 2 == 0 else chr(i - 32) for i in range(122, 96, -1))
+print("{}".format(alphabet), end='')
