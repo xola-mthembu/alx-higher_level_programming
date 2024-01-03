b36c1cab5d4cfca391a210d4909caea2ce7b12c2
xola-mthembu <xola.mthembu@gmail.com>
2024-01-02 16:35:26 -0800

    7-islower.py

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -3,3 +3,9 @@
 def islower(c):
     # Check if the character 'c' is lowercase
     return ('a' <= c <= 'z')
+
+if __name__ == "__main__":
+    test_cases = ['a', 'H', 'A', '3', 'g', '"']
+    for case in test_cases:
+        result = "lower" if islower(case) else "upper"
+        print("{} is {}".format(case, result))
