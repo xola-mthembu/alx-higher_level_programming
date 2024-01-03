d3c1a2236db40fb244ab7e519cc9cc2d2103ec0c
xola-mthembu <xola.mthembu@gmail.com>
2024-01-02 16:15:02 -0800

    3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 
-# Constructing the alphabet string without 'e' and 'q' using list comprehension
-alphabet = [chr(c) for c in range(97, 123) if c != 101 and c != 113]
-
 # Using a single print function with string format to print the entire alphabet string
-print("".join(alphabet))
+print("".join(chr(c) for c in range(97, 123) if c != 101 and c != 113), end='')
