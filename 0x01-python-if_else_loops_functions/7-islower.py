823ac386d8c67cd6bfe410c5f5bb8184af78054f
xola-mthembu <xola.mthembu@gmail.com>
2024-01-02 16:33:50 -0800

    7-islower.py

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
-def islower(c)i:
-    # Check if the Unicode code point of c is in the range of lowercase letters
-    return ord('a') <= ord(c) <= ord('z')
+def islower(c):
+    # Check if the character 'c' is lowercase
+    return ('a' <= c <= 'z')
