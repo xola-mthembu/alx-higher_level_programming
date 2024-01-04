270cd00a53f45812508b2a7ab4be07be482a875a
xola-mthembu <xola.mthembu@gmail.com>
2024-01-04 10:56:45 -0800

    3-infinite_add.py update

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 from sys import argv
 
+
 if __name__ == "__main__":
     sum_of_args = sum(int(arg) for arg in argv[1:])  # Sum all arguments converted to integers
     print(sum_of_args)
