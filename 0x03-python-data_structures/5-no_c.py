bb56078ba783c312ccdb22a94c3fc424ddff02ba
xola-mthembu <xola.mthembu@gmail.com>
2024-01-07 11:10:24 -0800

    5-no_c.py

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+def no_c(my_string):
+    """
+    Removes all characters 'c' and 'C' from a string.
+    """
+    return ''.join(ch for ch in my_string if ch not in ['c', 'C'])
