242ee1e8c5825e71002f66f6d0b0bb71a19cd881
xola-mthembu <xola.mthembu@gmail.com>
2024-01-08 15:05:04 -0800

    5-no_c.py updated

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,14 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+
 def no_c(my_string):
     """
-    Removes all characters 'c' and 'C' from a string.
+    This function removes all characters 'c' and 'C' from a string.
+    It returns the new string.
     """
-    return ''.join(ch for ch in my_string if ch != 'c' and ch != 'C')
+    return ''.join(char for char in my_string if char not in 'cC')
+
+if __name__ == "__main__":
+    # Example usage
+    print(no_c("Best School"))
+    print(no_c("Chicago"))
+    print(no_c("C is fun!"))
