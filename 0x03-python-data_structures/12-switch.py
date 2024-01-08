fb945349711e0195ea6a7d3e2ebef1ead0f569a7
xola-mthembu <xola.mthembu@gmail.com>
2024-01-08 15:12:31 -0800

    12-switch.py updated

--- 0x03-python-data_structures/12-switch.py
+++ 0x03-python-data_structures/12-switch.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# This script switches the values of a and b
-a, b = 89, 10
+a = 89
+b = 10
 a, b = b, a
-print("a={} - b={}".format(a, b))
+print("a={:d} - b={:d}".format(a, b))
