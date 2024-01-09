6964e4fb8b24a9bc30fbc0600f2f62cdc5481807
xola-mthembu <xola.mthembu@gmail.com>
2024-01-09 07:30:23 -0800

    13-is_palindrome.c updated

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,31 +1,49 @@
 #include "lists.h"
-#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
  * is_palindrome - checks if a singly linked list is a palindrome
  * @head: double pointer to the head of the list
+ *
  * Return: 0 if it is not a palindrome, 1 if it is a palindrome
  */
 int is_palindrome(listint_t **head)
 {
-    listint_t *current = *head;
-    int values[2048];
-    int i = 0, j;
+    listint_t *fast = *head, *slow = *head;
+    listint_t *prev = NULL, *temp = NULL, *second_half;
 
-    if (!head || !*head || !(*head)->next)
+    if (head == NULL || *head == NULL || (*head)->next == NULL)
         return (1);
 
-    while (current)
+    /* Find the middle and reverse the second half */
+    while (fast != NULL && fast->next != NULL)
     {
-        values[i++] = current->n;
-        current = current->next;
+        fast = fast->next->next;
+        temp = slow->next;
+        slow->next = prev;
+        prev = slow;
+        slow = temp;
     }
 
-    for (j = 0, i--; j <= i; j++, i--)
+    second_half = (fast == NULL) ? slow : slow->next;
+    temp = (fast == NULL) ? prev : slow;
+
+    /* Compare the first half and the reversed second half */
+    while (temp != NULL && second_half != NULL)
     {
-        if (values[j] != values[i])
+        if (temp->n != second_half->n)
+        {
+            /* Not a palindrome, restore the list and return 0 */
+            slow->next = prev;
             return (0);
+        }
+        temp = temp->next;
+        second_half = second_half->next;
     }
+
+    /* Restore the list */
+    slow->next = prev;
 
     return (1);
 }
