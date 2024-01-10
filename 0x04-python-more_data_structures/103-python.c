bf62950f35141bdc21b988d8af816852413a3bd5
xola-mthembu <xola.mthembu@gmail.com>
2024-01-10 14:56:50 -0800

    103-python.c

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,53 +1,62 @@
 #define PY_SSIZE_T_CLEAN
 #include <Python.h>
-#include <object.h>
 #include <listobject.h>
 #include <bytesobject.h>
+#include <stdio.h>
 
 void print_python_list(PyObject *p)
 {
-    Py_ssize_t size, allocated, i;
-    PyObject *item;
+	Py_ssize_t size, alloc, i;
+	PyObject *item;
 
-    printf("[*] Python list info\n");
-    if (!PyList_Check(p)) {
-        printf("  [ERROR] Invalid List Object\n");
-        return;
-    }
+	printf("[*] Python list info\n");
+	if (!PyList_Check(p))
+	{
+		printf("  [ERROR] Invalid List Object\n");
+		return;
+	}
 
-    size = PyList_Size(p);
-    allocated = ((PyListObject *)p)->allocated;
+	size = PyList_Size(p);
+	alloc = ((PyListObject *)p)->allocated;
 
-    printf("[*] Size of the Python List = %zd\n", size);
-    printf("[*] Allocated = %zd\n", allocated);
+	printf("[*] Size of the Python List = %zd\n", size);
+	printf("[*] Allocated = %zd\n", alloc);
 
-    for (i = 0; i < size; i++) {
-        item = PyList_GetItem(p, i);
-        printf("Element %zd: %s\n", i, item->ob_type->tp_name);
-        if (PyBytes_Check(item))
-            print_python_bytes(item);
-    }
+	for (i = 0; i < size; i++)
+	{
+		item = PyList_GetItem(p, i);
+		printf("Element %zd: %s\n", i, Py_TYPE(item)->tp_name);
+	}
 }
 
 void print_python_bytes(PyObject *p)
 {
-    Py_ssize_t size, i;
-    char *trying_str = NULL;
+	Py_ssize_t size, i;
+	char *string;
 
-    printf("[.] bytes object info\n");
-    if (!PyBytes_Check(p)) {
-        printf("  [ERROR] Invalid Bytes Object\n");
-        return;
-    }
+	printf("[.] bytes object info\n");
+	if (!PyBytes_Check(p))
+	{
+		printf("  [ERROR] Invalid Bytes Object\n");
+		return;
+	}
 
-    size = PyBytes_Size(p);
-    trying_str = PyBytes_AsString(p);
+	size = PyBytes_Size(p);
+	string = PyBytes_AsString(p);
 
-    printf("  size: %zd\n", size);
-    printf("  trying string: %s\n", trying_str ? trying_str : "N/A");
+	printf("  size: %zd\n", size);
+	printf("  trying string: %s\n", string);
+	printf("  first %zd bytes:", (size < 10) ? size + 1 : 10);
 
-    printf("  first %zd bytes:", (size < 10) ? size + 1 : 10);
-    for (i = 0; i < size && i < 10; i++)
-        printf(" %02hhx", ((char *)p)[i]);
-    printf("\n");
+	for (i = 0; i < size && i < 10; i++)
+	{
+		printf(" %02x", string[i] & 0xff);
+	}
+
+	printf("\n");
+}
+
+int main(void)
+{
+	return (0);
 }
