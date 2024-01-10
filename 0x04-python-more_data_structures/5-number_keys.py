91bae75e0796785eaa7f52642e556854dccdd603
xola-mthembu <xola.mthembu@gmail.com>
2024-01-10 12:19:35 -0800

    5-number_keys.py

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def number_keys(a_dictionary):
+    """
+    Returns the number of keys in a dictionary.
+    Args:
+    a_dictionary (dict): The dictionary to analyze.
+    Returns:
+    int: The number of keys in the dictionary.
+    """
+    return len(a_dictionary)
