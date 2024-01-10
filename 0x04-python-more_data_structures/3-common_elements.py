9ba38c556af3c183fb0c0cfa6f21d6319d26165c
xola-mthembu <xola.mthembu@gmail.com>
2024-01-10 12:05:35 -0800

    3-common_elements.py

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def common_elements(set_1, set_2):
+    """
+    Returns a set of common elements in two sets.
+    Args:
+    set_1 (set): The first set.
+    set_2 (set): The second set.
+    Returns:
+    set: Set of common elements.
+    """
+    return set_1 & set_2
