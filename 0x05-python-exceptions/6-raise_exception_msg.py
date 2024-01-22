b5347b33a6db85daa343a01f55a1ac2ce14ff85a
xola-mthembu <xola.mthembu@gmail.com>
2024-01-22 17:59:30 +0000

    6-raise_exception_msg.py

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def raise_exception_msg(message=""):
+    """
+    Raises a NameError exception with a custom message.
+    Args:
+        message (str): The message to be included in the exception.
+    """
+    raise NameError(message)
