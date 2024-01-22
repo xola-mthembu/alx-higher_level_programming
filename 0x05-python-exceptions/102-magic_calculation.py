03a550919b32f35122923dc7dd23094143a97a9b
xola-mthembu <xola.mthembu@gmail.com>
2024-01-22 19:02:40 +0000

    102-magic_calculation.py updated

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -4,9 +4,8 @@ def magic_calculation(a, b):
         try:
             if i > a:
                 raise Exception("Too far")
-            else:
-                result += a ** b / i
-        except:
+            result += a ** b / i
+        except Exception:
             result = a + b
             break
     return result
