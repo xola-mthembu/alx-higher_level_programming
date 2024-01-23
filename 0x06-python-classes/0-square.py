0e3e57405af3139c860c789efcda08b3b478c47a
xola-mthembu <xola.mthembu@gmail.com>
2024-01-23 15:24:51 +0000

    0-square.py

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module for Square class.
+
+This module provides a simple Square class with no functionality.
+"""
+
+class Square:
+    """An empty class that defines a square.
+
+    This class serves as a foundation for future more complex behavior.
+    """
+    pass
