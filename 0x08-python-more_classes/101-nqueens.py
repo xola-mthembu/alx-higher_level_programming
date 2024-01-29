91a4667a4b96056a8d9cca4b55a4bb2d7de0aadf
xola-mthembu <xola.mthembu@gmail.com>
2024-01-29 10:44:42 -0800

    101-nqueens.py updated

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,51 +1,51 @@
 #!/usr/bin/python3
 """
-N-queens problem solver
+N Queens puzzle solution
 """
 
 import sys
 
 
-def is_safe(board, row, col):
+def is_safe(board, row, col, n):
     """
-    Check if it's safe to place a queen at position (row, col)
+    Check if it's safe to place a queen at board[row][col]
     """
-    # Check the row on the left side
+    # Check in the same row to the left
     for i in range(col):
         if board[row][i] == 1:
             return False
 
-    # Check upper diagonal on the left side
+    # Check upper diagonal on left side
     for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
         if board[i][j] == 1:
             return False
 
-    # Check lower diagonal on the left side
-    for i, j in zip(range(row, len(board)), range(col, -1, -1)):
+    # Check lower diagonal on left side
+    for i, j in zip(range(row, n, 1), range(col, -1, -1)):
         if board[i][j] == 1:
             return False
 
     return True
 
 
-def solve_nqueens_util(board, col):
+def solve_nqueens_util(board, col, n, solutions):
     """
-    Utility function to solve N-queens problem using backtracking
+    Utilizes backtracking to solve N Queens problem
     """
-    if col == len(board):
-        print([[i, j] for i, row in enumerate(board) for j, value in enumerate(row) if value == 1])
+    if col == n:
+        solutions.append([[i, row.index(1)] for i, row in enumerate(board)])
         return
 
-    for i in range(len(board)):
-        if is_safe(board, i, col):
+    for i in range(n):
+        if is_safe(board, i, col, n):
             board[i][col] = 1
-            solve_nqueens_util(board, col + 1)
+            solve_nqueens_util(board, col + 1, n, solutions)
             board[i][col] = 0
 
 
 def solve_nqueens(n):
     """
-    Solve N-queens problem for a given board size N
+    Solve N Queens problem
     """
     if not isinstance(n, int):
         print("N must be a number")
@@ -56,17 +56,21 @@ def solve_nqueens(n):
         sys.exit(1)
 
     board = [[0 for _ in range(n)] for _ in range(n)]
-    solve_nqueens_util(board, 0)
+    solutions = []
+    solve_nqueens_util(board, 0, n, solutions)
+
+    for solution in solutions:
+        print(solution)
 
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: nqueens N")
+        print("Usage: ./101-nqueens.py N")
         sys.exit(1)
 
     try:
-        n = int(sys.argv[1])
-        solve_nqueens(n)
+        N = int(sys.argv[1])
+        solve_nqueens(N)
     except ValueError:
         print("N must be a number")
         sys.exit(1)
