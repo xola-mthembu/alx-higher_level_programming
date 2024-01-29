3d240d03eea7541e3fe6eb9b3483db709e5fd0d5
xola-mthembu <xola.mthembu@gmail.com>
2024-01-29 10:10:04 -0800

    2-rectangle.py fixed

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,14 +1,23 @@
+#!/usr/bin/python3
+"""Module containing the Rectangle class"""
+
+
 class Rectangle:
+    """Rectangle class with width, height, area, and perimeter methods"""
+
     def __init__(self, width=0, height=0):
+        """Initialize a new Rectangle instance"""
         self.width = width
         self.height = height
 
     @property
     def width(self):
+        """Getter method for width"""
         return self.__width
 
     @width.setter
     def width(self, value):
+        """Setter method for width"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -17,10 +26,12 @@ class Rectangle:
 
     @property
     def height(self):
+        """Getter method for height"""
         return self.__height
 
     @height.setter
     def height(self, value):
+        """Setter method for height"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
@@ -28,7 +39,11 @@ class Rectangle:
         self.__height = value
 
     def area(self):
+        """Return the area of the rectangle"""
         return self.__width * self.__height
 
     def perimeter(self):
+        """Return the perimeter of the rectangle"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
         return 2 * (self.__width + self.__height)
