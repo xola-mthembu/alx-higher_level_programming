9fdf93df7bc75bd0061164c8e64a846a8cba9560
xola-mthembu <xola.mthembu@gmail.com>
2024-01-30 09:56:02 -0800

    Task5

---
+++ 0x07-python-test_driven_development/6-max_integer.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Module to find the max integer in a list."""
+
+
+def max_integer(list=[]):
+    """Function to find and return the max integer in a list of integers.
+    If the list is empty, the function returns None.
+    """
+    if len(list) == 0:
+        return None
+    for element in list:
+        if not isinstance(element, int):
+            raise TypeError("All elements must be integers")
+    result = list[0]
+    for i in range(1, len(list)):
+        if list[i] > result:
+            result = list[i]
+    return result
