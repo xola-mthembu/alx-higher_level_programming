d02d8cf839341151ac0473e98e09b31ecf2049b4
xola-mthembu <xola.mthembu@gmail.com>
2024-02-05 14:44:44 -0800

    10-square.py

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,33 +1,32 @@
 #!/usr/bin/python3
 """
-Module 10-square - Contains a class Square that inherits from Rectangle.
+Module 10-square - Contains a class Square that inherits from Rect.
 """
 
 Rectangle = __import__('9-rectangle').Rectangle
 
+
 class Square(Rectangle):
     """
-    A class that represents a square, inheriting from Rectangle.
+    Represents a square, inheriting from Rectangle.
     """
     def __init__(self, size):
         """
-        Initialize a new Square.
-
-        Args:
-            size (int): The size of the square sides.
+        Initialize a new Square with size validated by int_validator.
         """
+        # Validate and initialize Square as Rectangle with equal sides
         self.integer_validator("size", size)
-        super().__init__(size, size)  # Initialize Rectangle with size as both width and height
+        super().__init__(size, size)
         self.__size = size
 
     def area(self):
         """
-        Calculate the area of the Square.
+        Calculate and return the area of the Square.
         """
         return self.__size ** 2
 
     def __str__(self):
         """
-        Return the Square description as a Rectangle format.
+        Return the Square description as Rectangle format.
         """
         return super().__str__()
