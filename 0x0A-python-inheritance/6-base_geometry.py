187175a7625916942277dbd96148222caad89c01
xola-mthembu <xola.mthembu@gmail.com>
2024-02-05 13:27:34 -0800

    6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module 6-base_geometry - contains a class BaseGeometry with a public instance
+method.
+"""
+
+class BaseGeometry:
+    """
+    A class BaseGeometry with a method to calculate the area, not yet implemented.
+    """
+    def area(self):
+        """
+        Raises an Exception with the message that the method is not implemented.
+        """
+        raise Exception("area() is not implemented")
