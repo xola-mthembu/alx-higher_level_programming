009db7f16d2a552fcd0798e9b4b06023887ad59a
xola-mthembu <xola.mthembu@gmail.com>
2024-02-12 09:47:17 -0800

    Task11

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -16,9 +16,20 @@ class Square(Rectangle):
         """
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
+
     def __str__(self):
         """
         Returns the string representation of the Square instance.
         """
         return "[Square] ({}) {}/{} - {}".format(
             self.id, self.x, self.y, self.width)
+
+    # Other methods from Rect (area, display, update) are inherited
