7bbe8d7a65981d39bc291498118d9cea75bdf33d
xola-mthembu <xola.mthembu@gmail.com>
2024-02-12 11:46:37 -0800

    Task18

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -10,25 +10,21 @@ class Base:
     _nb_objects = 0
 
     def __init__(self, id=None):
-        """Initialize a new Base instance."""
         if id is not None:
             self.id = id
         else:
             Base._nb_objects += 1
             self.id = Base._nb_objects
 
-    @staticmethod
-    def to_json_string(list_dictionaries):
-        """Return the JSON string representation of list_dictionaries."""
-        if not list_dictionaries:
-            return "[]"
-        return json.dumps(list_dictionaries)
+    # Include to_json_string and from_json_string methods here...
 
-    @staticmethod
-    def from_json_string(json_string):
-        """Returns the list represented by json_string."""
-        if json_string is None or json_string == "":
-            return []
-        return json.loads(json_string)
+    @classmethod
+    def create(cls, **dictionary):
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1)
+        elif cls.__name__ == "Square":
+            dummy = cls(1)
+        dummy.update(**dictionary)
+        return dummy
 
 # Continue with other previously defined methods...
