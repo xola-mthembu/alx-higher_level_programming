c20db998298fad12390417f6530281cc2d403d96
xola-mthembu <xola.mthembu@gmail.com>
2024-02-12 10:01:43 -0800

    Task12

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -18,10 +18,16 @@ class Square(Rectangle):
 
     @property
     def size(self):
+        """
+        Gets the size of the Square.
+        """
         return self.width
 
     @size.setter
     def size(self, value):
+        """
+        Sets the size of the Square, applying the same validation as width.
+        """
         self.width = value
         self.height = value
 
@@ -30,6 +36,25 @@ class Square(Rectangle):
         Returns the string representation of the Square instance.
         """
         return "[Square] ({}) {}/{} - {}".format(
-            self.id, self.x, self.y, self.width)
+            self.id, self.x, self.y, self.size)
 
-    # Other methods from Rect (area, display, update) are inherited
+    def update(self, *args, **kwargs):
+        """
+        Updates the Square attributes with non-keyword
+        arguments first, then keyword arguments.
+        """
+        attrs = ['id', 'size', 'x', 'y']
+        if args and len(args) > 0:
+            for attr, arg in zip(attrs, args):
+                if attr == 'size':
+                    setattr(self, 'width', arg)
+                    setattr(self, 'height', arg)
+                else:
+                    setattr(self, attr, arg)
+        elif kwargs:
+            for key, value in kwargs.items():
+                if key == 'size':
+                    setattr(self, 'width', value)
+                    setattr(self, 'height', value)
+                elif key in attrs:
+                    setattr(self, key, value)
