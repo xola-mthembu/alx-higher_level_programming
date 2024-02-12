ada6613d0087ff2a96365db453b67e54c752d6d4
xola-mthembu <xola.mthembu@gmail.com>
2024-02-12 07:32:35 -0800

    Task1

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,10 +1,24 @@
 #!/usr/bin/python3
 """
-This module provides a Base class.
+This module provides a Base class for all models in the project.
 """
+
 
 class Base:
     """
-    Base class for future models.
+    A base class for all models.
     """
-    pass
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+        Initialize a new Base instance.
+
+        Args:
+            id (int): The identity of the new Base instance.
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
