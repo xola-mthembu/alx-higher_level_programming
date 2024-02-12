c759b46b9cbcd3e9482bc64e2aa18f43ee4a2224
xola-mthembu <xola.mthembu@gmail.com>
2024-02-12 08:37:48 -0800

    Task6

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -88,3 +88,10 @@ class Rectangle(Base):
         """
         for _ in range(self.height):
             print("#" * self.width)
+
+    def __str__(self):
+        """
+        Returns the string representation of the Rectangle instance.
+        """
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(
+            self.id, self.x, self.y, self.width, self.height)
