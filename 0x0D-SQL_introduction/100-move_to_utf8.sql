59643387fb9c9b53f11220155bad19ab787c20b2
xola-mthembu <xola.mthembu@gmail.com>
2024-03-12 13:23:17 -0700

    100-move_to_utf8.sql updated

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,9 +1,10 @@
--- Convert the database to UTF8
+-- Convert hbtn_0c_0 database to UTF8 (utf8mb4, collate utf8mb4_unicode_ci)
+USE hbtn_0c_0;
 ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 
--- Convert the table to UTF8
+-- Convert first_table table to UTF8 (utf8mb4, collate utf8mb4_unicode_ci)
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- Specifically convert the name column to UTF8
-ALTER TABLE first_table CHANGE name name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- Convert name field in first_table to UTF8 (utf8mb4, collate utf8mb4_unicode_ci)
+ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
