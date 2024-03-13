749cc451bc8e89089f3284befa3b78aaed8341f4
xola-mthembu <xola.mthembu@gmail.com>
2024-03-13 08:55:05 -0700

    13-count_shows_by_genre.sql

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,9 +1,8 @@
 -- This script lists genres and the number of shows for each genre
 
-SELECT genre, COUNT(*) AS number_of_shows
-FROM genres
-INNER JOIN shows ON genres.id = shows.genre_id
-GROUP BY genre
-HAVING COUNT(*) > 0
+SELECT g.genre, COALESCE(COUNT(s.id), 0) AS number_of_shows
+FROM genres g
+LEFT JOIN shows s ON g.id = s.genre_id
+GROUP BY g.genre
 ORDER BY number_of_shows DESC;
 
