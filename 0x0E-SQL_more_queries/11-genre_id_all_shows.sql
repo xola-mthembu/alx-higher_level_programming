cda1768a2732340714ef9b4708a38e2f268c1e13
xola-mthembu <xola.mthembu@gmail.com>
2024-03-13 07:19:21 -0700

    11-genre_id_all_shows.sql

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,7 @@
+-- 11-genre_id_all_shows.sql: Lists all shows with their genre IDs, showing NULL for shows without genres.
+
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+
