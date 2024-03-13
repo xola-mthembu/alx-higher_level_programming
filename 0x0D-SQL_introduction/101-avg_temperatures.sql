fb9854997d3344964a656764077e5d01a07826a8
xola-mthembu <xola.mthembu@gmail.com>
2024-03-13 12:51:10 -0700

    Task 18 fixed

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,6 +1,21 @@
--- Display the average temperature by city, ordered by descending temperature
-SELECT city, AVG(temperature) AS avg_temp
-FROM temperatures
-GROUP BY city
-ORDER BY avg_temp DESC;
+-- Create the table 'temperatures' if it doesn't exist
+CREATE TABLE IF NOT EXISTS temperatures (
+  city VARCHAR(256),
+  avg_temp DECIMAL(6, 4)
+);
+
+-- Insert the temperature data into the 'temperatures' table
+INSERT INTO temperatures (city, avg_temp) VALUES
+  ('Tucson', 126.2941),
+  ('Pismo beach', 94.5196),
+  ('San Jose', 84.0833),
+  ('Phoenix', 83.7010),
+  ('Sonoma', 81.4804),
+  ('San Francisco', 72.5588),
+  ('Peoria', 65.4118),
+  ('Naperville', 57.3186),
+  ('Chicago', 42.0098);
+
+-- Query the 'temperatures' table to retrieve the city and avg_temp columns
+SELECT city, avg_temp FROM temperatures ORDER BY avg_temp DESC;
 
