bbb85a4dc9838e0bfbcbf81d69589d5145363ccd
xola-mthembu <xola.mthembu@gmail.com>
2024-03-13 12:34:35 -0700

    Task 17 fixed

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -7,6 +7,6 @@ USE `hbtn_0c_0`;
 -- Convert first_table table to UTF8
 ALTER TABLE `first_table` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- Convert name field in first_table to UTF8
+-- Modify the name column to use the correct collation
 ALTER TABLE `first_table` MODIFY `name` VARCHAR(256) COLLATE utf8mb4_unicode_ci;
 
