2ef0077c278124050cda2a3fa95f8cd828553c46
xola-mthembu <xola.mthembu@gmail.com>
2024-03-13 13:16:51 -0700

    Task 20 fixed

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,5 +1,5 @@
 -- Display the maximum temperature for each state, ordered by state name
-SELECT state, MAX(temperature) AS max_temp
+SELECT SUBSTRING(city, LOCATE(',', city) + 1) AS state, MAX(value) AS max_temp
 FROM temperatures
 GROUP BY state
 ORDER BY state;
