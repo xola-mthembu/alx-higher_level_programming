ed61a8b37790196b6a36bcbde6ec5a62dc6e5f52
xola-mthembu <xola.mthembu@gmail.com>
2024-03-13 06:15:57 -0700

    0-privileges.sql

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,8 @@
+-- 0-privileges.sql: Lists all privileges of user_0d_1 and user_0d_2
+
+-- Show privileges for user_0d_1
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- Show privileges for user_0d_2
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
+
