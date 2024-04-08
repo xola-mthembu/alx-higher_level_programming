1243a474ef5be20346f557062c3882197d8e736f
xola-mthembu <xola.mthembu@gmail.com>
2024-04-08 10:33:58 -0700

    8-square.js

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// This script prints a square of size "size" using the character "X"
+const size = parseInt(process.argv[2]);
+
+if (isNaN(size)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < size; i++) {
+    console.log('X'.repeat(size));
+  }
+}
