5d404a57c93e1cbc0c047686b331d5116177cbcc
xola-mthembu <xola.mthembu@gmail.com>
2024-04-08 09:45:15 -0700

    2-arguments.js

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// This script prints a message depending on the number of arguments passed
+const args = process.argv;
+
+if (args.length === 2) {
+  console.log('No argument');
+} else if (args.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
