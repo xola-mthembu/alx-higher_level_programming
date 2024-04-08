a022c48acc00af92097782e10dd69ade09ba1050
xola-mthembu <xola.mthembu@gmail.com>
2024-04-08 11:57:44 -0700

    13-add.js

---
+++ 0x12-javascript-warm_up/13-add.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// This module defines a function that adds two integers
+
+function add (a, b) { // Added space before the parentheses
+  return a + b;
+}
+
+module.exports.add = add;
