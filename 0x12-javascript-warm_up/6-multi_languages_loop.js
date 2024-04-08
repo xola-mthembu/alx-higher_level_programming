f64bb34a8262fc2e24f76d3f659db0aa61942790
xola-mthembu <xola.mthembu@gmail.com>
2024-04-08 10:17:47 -0700

    6. Loop to languages

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// This script prints 3 lines using an array of strings and a loop
+const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let i = 0; i < languages.length; i++) {
+  console.log(languages[i]);
+}
