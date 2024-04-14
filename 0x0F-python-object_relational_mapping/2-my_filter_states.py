7cf408801fe93f950c7f634c25dbe69681e8ac2e
xola-mthembu <xola.mthembu@gmail.com>
2024-04-14 15:38:19 -0700

    2. Filter states by user input fixed

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,37 +1,37 @@
 #!/usr/bin/python3
 """
-This script takes in an argument and displays all values in the states table
-of hbtn_0e_0_usa where name matches the argument.
+Script that takes in an argument and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument.
 """
-import MySQLdb
 import sys
+import MySQLdb
+
 
 if __name__ == "__main__":
-    # Check the command line arguments
-    if len(sys.argv) != 5:
-        print("Usage: {} <mysql_username> <mysql_password> <database_name> \
-<state_name>".format(sys.argv[0]))
-        sys.exit(1)
+    # Get the command line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
 
-    # Connect to the database
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=sys.argv[1],
-        password=sys.argv[2],
-        database=sys.argv[3]
-    )
+    # Connect to the MySQL server
+    db = MySQLdb.connect(host="localhost", port=3306, user=username,
+                         passwd=password, db=db_name)
 
     # Create a cursor object
-    cursor = db.cursor()
-    # Execute the SQL query with parameter
-    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
-    cursor.execute(query, (sys.argv[4],))
+    cur = db.cursor()
 
-    # Fetch and display the results
-    for row in cursor.fetchall():
+    # Execute the SQL query with user input
+    cur.execute("SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC",
+                (state_name,))
+
+    # Fetch all the rows
+    rows = cur.fetchall()
+
+    # Display the results
+    for row in rows:
         print(row)
 
-    # Close the cursor and the connection
-    cursor.close()
+    # Close the cursor and database connection
+    cur.close()
     db.close()
