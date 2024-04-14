913fac7aefcd46a77887060f8e371cdecdd4d2b3
xola-mthembu <xola.mthembu@gmail.com>
2024-04-14 15:18:36 -0700

    15. City relationship updated

---
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Contains the class definition of a City.
+"""
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from relationship_state import Base
+
+
+class City(Base):
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id', ondelete='CASCADE'))
+    state = relationship("State", back_populates="cities")
