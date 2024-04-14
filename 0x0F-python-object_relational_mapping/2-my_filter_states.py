4db34a4d07dd1f7d5e6a355df5eb73cc5afb3bb8
xola-mthembu <xola.mthembu@gmail.com>
2024-04-14 12:39:56 -0700

    2-my_filter_states.py updated

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,41 +1,37 @@
 #!/usr/bin/python3
 """
-This module filters states by exact name match from the database hbtn_0e_0_usa.
+This script takes in an argument and displays all values in the states table
+of hbtn_0e_0_usa where name matches the argument.
 """
-
-import sys
 import MySQLdb
+import sys
 
-def filter_states_by_user_input():
-    """
-    Connects to the database and filters states by user input.
-    """
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    state_searched = sys.argv[4]
+if __name__ == "__main__":
+    # Check the command line arguments
+    if len(sys.argv) != 5:
+        print("Usage: {} <mysql_username> <mysql_password> <database_name> \
+<state_name>".format(sys.argv[0]))
+        sys.exit(1)
 
-    # Connect to the MySQL database
-    db = MySQLdb.connect(host="localhost", port=3306, user=username,
-                         passwd=password, db=database)
+    # Connect to the database
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        password=sys.argv[2],
+        database=sys.argv[3]
+    )
 
     # Create a cursor object
     cursor = db.cursor()
-
-    # Secure way to format SQL queries with user input
+    # Execute the SQL query with parameter
     query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
-    cursor.execute(query, (state_searched,))
+    cursor.execute(query, (sys.argv[4],))
 
-    # Fetch all the rows in a list of lists.
-    results = cursor.fetchall()
-
-    # Print the results
-    for state in results:
-        print(state)
+    # Fetch and display the results
+    for row in cursor.fetchall():
+        print(row)
 
     # Close the cursor and the connection
     cursor.close()
     db.close()
-
-if __name__ == "__main__":
-    filter_states_by_user_input()
