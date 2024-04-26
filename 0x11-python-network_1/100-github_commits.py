73dc926122b5001dad0795a5e2a2654e22b7116e
xola-mthembu <xola.mthembu@gmail.com>
2024-04-26 14:30:57 -0700

    Task 10 updated

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-Script retrieves the last 10 commits of a specified repo and usr
-from GitHub's API and lists them from the most recent to oldest in the format:
+Script retrieves the last 10 commits of a specified repo and user
+from GitHub's API and lists from most recent to oldest in the format:
 <sha>: <author name>.
 """
 import requests
@@ -13,21 +13,17 @@ if __name__ == "__main__":
         print("Usage: ./100-github_commits.py <repo name> <owner>")
         sys.exit(1)
 
-    repo = sys.argv[1]
-    owner = sys.argv[2]
-    url = f"https://api.github.com/repos/{owner}/{repo}/commits"
+    repo_name = sys.argv[1]
+    owner_name = sys.argv[2]
+    url = f"https://api.github.com/repos/{owner_name}/{repo_name}/commits?per_page=10"
 
-    try:
-        response = requests.get(url)
-        if response.status_code != 200:
-            print(f"Error fetching commits: {response.status_code}")
-            sys.exit(1)
-
-        commits = response.json()
-        for commit in commits[:10]:
-            sha = commit.get('sha')
-            author = commit.get('commit').get('author').get('name')
-            print(f"{sha}: {author}")
-    except requests.exceptions.RequestException as e:
-        print(f"An error occurred: {e}")
+    response = requests.get(url)
+    if response.status_code != 200:
+        print(f"Error fetching commits: {response.status_code}")
         sys.exit(1)
+
+    commits = response.json()
+    for commit in commits:
+        sha = commit.get('sha')
+        author_name = commit.get('commit').get('author').get('name')
+        print(f"{sha}: {author_name}")
