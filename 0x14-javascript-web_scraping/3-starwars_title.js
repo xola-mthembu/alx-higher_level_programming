fb5497ca54bcb76c8c0d476a9b9afc4880a651f1
xola-mthembu <xola.mthembu@gmail.com>
2024-05-21 09:09:05 -0700

    Task 3: Print Star Wars movie title by episode number

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const request = require('request');
+
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request(url, (err, response, body) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(JSON.parse(body).title);
+});
