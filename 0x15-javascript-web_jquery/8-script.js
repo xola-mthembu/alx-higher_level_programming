43dd704ab4cbf15bdb4270b208f22798a921c577
xola-mthembu <xola.mthembu@gmail.com>
2024-05-31 03:58:21 -0700

    8. Star Wars movies

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+/* global $ */
+
+$(document).ready(function () {
+  $.get('https://swapi-api.alx-tools.com/api/films/?format=json', function (data) {
+    data.results.forEach(function (film) {
+      $('#list_movies').append('<li>' + film.title + '</li>');
+    });
+  });
+});
